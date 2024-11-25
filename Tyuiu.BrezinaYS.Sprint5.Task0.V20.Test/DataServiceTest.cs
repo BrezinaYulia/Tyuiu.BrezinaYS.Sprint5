39fefa1e01f23efc3a92e75faf02fa40dd54c135
ulabrezina <ulabrezina@gmail.com>
2024-11-25 21:59:39 +0500

    Task0

--- Tyuiu.BrezinaYS.Sprint5.Task0.V20.Test/DataServiceTest.cs
+++ Tyuiu.BrezinaYS.Sprint5.Task0.V20.Test/DataServiceTest.cs
@@ -8,8 +8,8 @@ namespace Tyuiu.BrezinaYS.Sprint5.Task0.V20.Test
         [TestMethod]
         public void TestMethod1()
         {
-            //string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
-            string path = @"C:\Users\Пользователь\source\repos\Tyuiu.BrezinaYS.Sprint5\Tyuiu.BrezinaYS.Sprint5.Task0.V5\bin\Debug\OutPutFileTask0.txt";
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            //string path = @"C:\Users\Пользователь\source\repos\Tyuiu.BrezinaYS.Sprint5\Tyuiu.BrezinaYS.Sprint5.Task0.V5\bin\Debug\OutPutFileTask0.txt";
             FileInfo fileinfo = new FileInfo(path);
             bool fileExists = fileinfo.Exists;
             bool wait = true;
