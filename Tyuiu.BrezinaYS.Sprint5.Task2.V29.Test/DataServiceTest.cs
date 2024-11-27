722ddf951c9b72e05c69b0339b5901aa69af8a86
ulabrezina <ulabrezina@gmail.com>
2024-11-28 01:09:17 +0500

    Task2

---
+++ Tyuiu.BrezinaYS.Sprint5.Task2.V29.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using Tyuiu.BrezinaYS.Sprint5.Task2.V29.Lib;
+
+namespace Tyuiu.BrezinaYS.Sprint5.Task2.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Пользователь\source\repos\Tyuiu.BrezinaYS.Sprint5\Tyuiu.BrezinaYS.Sprint5.Task1.V25\bin\Debug\net8.0\OutPutFileTask1.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
